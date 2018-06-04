bfb7b0d58f3774ff1156f2629cfd0310a558c2d8
Istudy618 <53629882@qq.com>
2018-06-04 16:37:12 +0800

    首次提交Git

---
+++ src/main/java/com/innyo/yysc/Application.java
@@ -0,0 +1,12 @@
+package com.innyo.yysc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
